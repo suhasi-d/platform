a3471f7f671350a075285c4a9befd5d8d771ff73
Suhasini <101970321+suhasi-d@users.noreply.github.com>
2022-04-18 10:36:35 +0530

    Update platform_setup.bat

--- platform_setup.bat
+++ platform_setup.bat
@@ -1,11 +1,10 @@
-# Set the Enviroment variables 
 
 
 set SHARED_VOLUME_PATH=C:\Code
 
 
 
-#Clean container and images
+
 
 docker-compose down
 
@@ -13,6 +12,6 @@ docker rm -f platform_dev
 
 
 
-# Bring up containers  
+
 
 docker-compose up -d
